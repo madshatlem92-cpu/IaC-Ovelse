46de3ae838ed384a5d5c3fce956a7635b6321ddc
madshatlem92-cpu <madshatlem92@gmail.com>
2026-05-22 18:42:28 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,13 +13,6 @@ terraform {
     }
   }
 }
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-}
 
 provider "azurerm" {
   features {}
@@ -29,4 +22,3 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
-# test
